73a9c5dab3e01fd2bed8f3d2f055c1f9ab3c65af
devari-el <arielrandis@gmail.com>
2024-10-05 16:13:09 -0300

    Debug na operação Saque, implementação de funções substituindo estruturas condicionais encadeiadas e implmentação de sistema de registro de data e hora nas transações

--- Sistema_Bancario.py
+++ Sistema_Bancario.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+import pytz
+
 opcao = 0
 saque = 0
 deposito = 0
@@ -6,55 +9,90 @@ saldo = 0
 limite_saque = 0
 
 menu = """ 
-------MENU------
+----------------MENU----------------
 
 [1] Extrato
-[2] Depósito
-[3] Saque
+[2] Saque
+[3] Depósito
 [4] Sair 
 
-----------------
+------------------------------------
 """
 
 extrato = """
-------EXTRATO------
+---------------EXTRATO---------------
 """
 
-while True:
-    print(menu)
-    opcao = int(input("Digite a opção escolhida... "))
+def registro_trans_data_hora():
 
-    if (opcao == 4):
-        print("\nObrigado por utilizar o RND Bank, satisfação em tê-lo como cliente!\n")
-        break
+    data_utc = datetime.now(pytz.utc)
+    sao_paulo = pytz.timezone('America/Sao_Paulo')
+    data_sp = data_utc.astimezone(sao_paulo)
+    data_formatada = data_sp.strftime("%d/%m/%Y %H:%M:%S")
+    return data_formatada
 
-    elif (opcao == 3):
-        if (limite_saque == 3):
-            print("\nVocê excedeu o número de saques! Volte em 24h.")
+def operacao_saque():
+
+    global limite_saque, saque, saldo, extrato
+
+    if limite_saque == 3:
+        print("\nVocê excedeu o número de saques! Volte em 24h.")
+    
+    else:
+        saque = int(input("\nDigite o valor que deseja sacar... \nMín: R$ 10,00\nMax R$ 500,00\nR$ "))
+
+        while saque < 10 or saque > 500:
+            saque = int(input("\nValor inválido!\nDigite o valor que deseja sacar... \n(Mín: R$ 10,00\nMax R$ 500,00)\nR$ "))
+
+        if saque > saldo:
+            print("Seu saldo é insuficiente! Digite outro valor ou deposite em sua conta.")
+        
         else:
-            saque = int(input("\nDigite o valor que deseja sacar... \n(Mín: R$ 10,00\nMax R$ 500,00)\nR$ "))
-
-            while (saque < 10 or saque > 500 or saque > saldo):
-                saque = int(input("\nValor inválido!\nDigite o valor que deseja sacar... \n(Mín: R$ 10,00\nMax R$ 500,00)\nR$ "))
-
             limite_saque +=1
             saldo -= saque      
-            extrato += (f"\n\nSaque........R$ {saque:.2f}") 
-            print("\nSaque realizado com sucesso!")   
+            extrato += (f"\n\nSaque - R$ {saque:.2f}          {registro_trans_data_hora()}") 
+            print("\nSaque realizado com sucesso!")
 
-    elif (opcao == 2):
-        deposito = int(input("\nDigite o valor à ser depositado...\n(Mín: R$ 10,00)\nR$ "))
+def operacao_deposito():
 
-        while (deposito < 10):
-            deposito = int(input("\nValor inválido!\nDigite o valor à ser depositado...\n(Mín: R$ 10,00)\nR$ "))
-    
-        saldo += deposito
-        extrato += (f"\n\nDepósito........R$ {deposito:.2f}")
-        print("\nDepósito realizado com sucesso!")
-    
-    elif (opcao == 1):
-        print (extrato, (f"\n\n\nSALDO ATUAL: R$ {saldo:.2f}"))
+    global deposito, saldo, extrato
 
+    deposito = int(input("\nDigite o valor à ser depositado...\n(Mín: R$ 10,00)\nR$ "))
+
+    while (deposito < 10):
+        deposito = int(input("\nValor inválido!\nDigite o valor à ser depositado...\n(Mín: R$ 10,00)\nR$ "))
+
+    saldo += deposito
+    extrato += (f"\n\nDepósito - R$ {deposito:.2f}          {registro_trans_data_hora()}")
+    print("\nDepósito realizado com sucesso!")
+
+def menu_interno():
+
+    global menu, opcao, extrato, saldo
+
+    while True:
+        
+        print(menu)
+        opcao = int(input("Digite a opção escolhida... "))
+
+        if (opcao == 1):
+            print (extrato, (f"\n\n\nSALDO ATUAL: R$ {saldo:.2f}          Atualizada em: {registro_trans_data_hora()}"))
+
+        elif (opcao == 2):
+            operacao_saque()
+
+        elif (opcao == 3):
+            operacao_deposito()
+        
+        elif (opcao == 4):
+            print("\nObrigado por utilizar o RND Bank, satisfação em tê-lo como cliente!\n")
+            break
+
+        else:
+            print("Opção inválida!")
+
+while True:
+    if (opcao == 4):
+        break
     else:
-        print("Opção inválida!")
-
+        (menu_interno())
